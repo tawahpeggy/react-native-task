cb9b5e0eb0aa845fc2a933f21d85c581cd685a33
tawahpeggy <tawahpeggy98@gmail.com>
2021-06-08 15:48:29 -0700

    react-native-internship task

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, {Component  } from "react";
+import {StyleSheet, View,Text } from "react-native";
+
+export default class App extends Component
+{
+  render(){
+    
+    return(
+      <view>
+        <Text>E V E N T S</Text>
+        <Text h5> 11TH</Text><br/>
+        <Text h5> NOV</Text>
+        <Text h6> Sunday Super Club</Text>
+        <Text> A culinary event with foods from all over the world. this is a weekly club you can join for a small fee.  Learn cooking methods from top chefs </Text>
+        <Text> &#43; Event Details</Text>
+        <Text h6>16:00-19:00</Text>
+        <Text> 144WElm St, chicago,IL 60610</Text>
+        <Text>USA</Text><br/>     
+        <hr/>
+        <Text h5> 13TH</Text><br/>
+        <Text h5> NOV</Text>
+        <Text h6> Evenings in the park</Text>
+        <Text> We invite you to listen to live bands play beautiful music in the park.  Enjoy the sounds of Laureen Davis and the kings, and enjoy wine taste...</Text>
+        <Text> &#43; Event Details</Text>
+        <Text h6>06:30-08:30</Text>
+        <Text> Estruary Park,</Text>
+        <hr/>
+        <Text h5> 15TH</Text><br/>
+        <Text h5> NOV</Text>
+        <Text h6> Morning Brews & Views</Text>
+        <Text> Enjoy free samples of out latest brews while overlooking the beautiful sites of our city Plenty of Iced and hot teas.plus gourmet coffee d...</Text>
+        <Text> &#43; Event Details</Text>
+        <Text h6>18:00-15:00</Text>
+        <Text> The Teapot and Brew Club</Text>
+        <hr/>
+        
+
+
+
+      </view>
+    );
+  }
+}
